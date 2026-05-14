700847c73882c9284378eae64f9cf5ae53b30852
Bohdan Moroziuk <bogdan7morozyuk@gmail.com>
2026-05-14 18:23:30 +0300

    chore: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+  ],
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: [
+    '~/assets/css/main.css',
+  ],
+
+  compatibilityDate: '2025-07-15',
+
+  typescript: {
+    typeCheck: true,
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
