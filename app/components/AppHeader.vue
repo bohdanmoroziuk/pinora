4a1d872c7df3dad8c21b3fd299e9121bc8aa9653
Bohdan Moroziuk <bogdan7morozyuk@gmail.com>
2026-05-16 12:59:30 +0300

    feat: add app header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="flex items-center gap-4 py-4">
+    <UInput
+      class="w-full"
+      icon="i-lucide-search"
+      size="md"
+      variant="outline"
+      placeholder="Search"
+    />
+
+    <AppUserButton />
+  </header>
+</template>
---
+++ app/components/AppUserButton.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+const isLoggedIn = ref(true)
+
+const items = ref<DropdownMenuItem[]>([
+  {
+    label: 'Profile',
+  },
+  {
+    label: 'Settings',
+  },
+  {
+    label: 'Logout',
+  },
+])
+</script>
+
+<template>
+  <div
+    v-if="isLoggedIn"
+    class="hidden sm:flex items-center gap-4"
+  >
+    <img
+      class="w-9 h-9 rounded-full object-cover"
+      src="/icons/no-avatar.png"
+      alt=""
+    >
+
+    <UDropdownMenu
+      :items="items"
+      :content="{
+        align: 'start',
+        side: 'bottom',
+        sideOffset: 8,
+      }"
+      :ui="{
+        content: 'w-24',
+      }"
+    >
+      <img
+        class="w-4 h-4 cursor-pointer"
+        src="/icons/arrow.svg"
+        alt=""
+      >
+    </UDropdownMenu>
+  </div>
+
+  <ULink
+    v-else
+    class="leading-none text-nowrap text-muted px-4 py-2 rounded-4xl hover:bg-[#f1f1f1]"
+    to="/"
+  >
+    Login / Sign up
+  </ULink>
+</template>
